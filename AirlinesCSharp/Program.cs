65175be48d97eb69f8d42537781a19b9e53f81aa
Ali <alibilge@yahoo.ca>
2020-05-15 01:37:55 -0600

    Assignment commit

---
+++ AirlinesCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirlinesCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
